c476d07162954d50f63fb3937c13ca091ca2f92f
auxmeet <mit35826@gmail.com>
2026-09-18 22:15:23 +0900

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,5 +6,5 @@ if [ -z "$MAGISK_VER_CODE" ] || [ "$MAGISK_VER_CODE" -lt "$MIN_MAGISK" ]; then
   abort "! Requires Magisk 26.4+ (current: ${MAGISK_VER:-unknown})"
 fi
 
-ui_print "- Infinix GSI brightness fix (Unisoc Tiger)"
+ui_print "- Infinix GSI brightness fix"
 ui_print "- Magisk $MAGISK_VER"
